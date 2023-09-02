c45eb2570d11d943758413a519d2190f40b31f1a
Gordon <scott_gordon28@hotmail.com>
2023-09-02 19:49:20 +1000

    working images

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
 
         String username = "pf_plann";
         String password = "uqchangeme";
@@ -21,6 +25,9 @@ public class Main {
                 "(ADDRESS = (PROTOCOL = TCP)(HOST = conman1.compute.dc.uq.edu.au)(PORT = 1521))" +
                 "(ADDRESS = (PROTOCOL = TCP)(HOST = conman2.compute.dc.uq.edu.au)(PORT = 1521))" +
                 "(CONNECT_DATA =(SERVICE_NAME = PFABPROD.SOE.UQ.EDU.AU)))";
+
+        List<Map<String, Object>> records = new ArrayList<>();
+
 
         try {
             Connection connection = DriverManager.getConnection(connectionURL, username, password);
@@ -64,38 +71,36 @@ public class Main {
                     "                      join afm.map_bl on map_bl.bl_id = bl.bl_id\n" +
                     "                      join afm.site on site.site_id = bl.site_id\n" +
                     "\n" +
-                    "                     where map_bl.arv != 0 and bl.site_id = '01'  order by bl.bl_id"; // Place your SQL query here.
+                    "                     where map_bl.arv != 0 and bl.site_id = '01'  order by bl.bl_id";
+
+
             PreparedStatement stmt = connection.prepareStatement(query);
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                String bl_id = rs.getString("bl_id");
+                Map<String, Object> record = new HashMap<>();
+                record.put("BL_ID", rs.getString("BL_ID"));
+                record.put("BLNAME", rs.getString("BLNAME"));
+                record.put("AREA_GROSS_INT", rs.getString("AREA_GROSS_INT"));
+                record.put("ARV", rs.getString("ARV"));
+                record.put("USE1", rs.getString("USE1"));
                 Blob eq_photo_blob = rs.getBlob("eq_photo");
-
-                // If the eq_photo column has data, save it to a file.
+                byte[] bytes = null;
                 if (eq_photo_blob != null) {
-                    InputStream inputStream = eq_photo_blob.getBinaryStream();
-                    FileOutputStream outputStream = new FileOutputStream("C:\\TMP\\itext\\" + bl_id + ".jpg");
-
-                    byte[] buffer = new byte[4096];
-                    int bytesRead = -1;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
-                    }
-
-                    inputStream.close();
-                    outputStream.close();
+                    bytes = eq_photo_blob.getBytes(1, (int) eq_photo_blob.length());
                 }
+                record.put("eq_photo_bytes", bytes);
+                records.add(record);
             }
 
             rs.close();
             stmt.close();
             connection.close();
-        } catch (SQLException | IOException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         String path = "C:\\TMP\\itext\\FontPdf.pdf";
-        PdfGenerator.generatePdf(path);
+        PdfGenerator.generatePdf(path, records);
     }
 }
--- src/main/java/PdfGenerator.java
+++ src/main/java/PdfGenerator.java
@@ -1,6 +1,5 @@
-
-
 import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
@@ -8,18 +7,28 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.io.IOException;
+import java.io.ByteArrayInputStream;
 
 public class PdfGenerator {
 
-    public static void generatePdf(String path) throws IOException {
+    public static void generatePdf(String path, List<Map<String, Object>> records) throws IOException, SQLException {
         PdfWriter pdfWriter = new PdfWriter(path);
         PdfDocument pdfDocument = new PdfDocument(pdfWriter);
         Document document = new Document(pdfDocument);
 
-        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
-        PdfFont boldFont = PdfFontFactory.createFont(FontConstants.TIMES_BOLD);
+        PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA);
+        PdfFont boldFont = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
 
         Text text1 = new Text("You are nice").setFont(font);
         Text text2 = new Text(" gordon").setFont(boldFont);
@@ -27,6 +36,53 @@ public class PdfGenerator {
         Paragraph paragraph = new Paragraph().add(text1).add(text2);
 
         document.add(paragraph);
+
+        int count = 0;
+        Table table = new Table(2);
+
+        for (Map<String, Object> record : records) {
+            String textData = String.format("BL_ID: %s\nBLNAME: %s\nAREA_GROSS_INT: %s\nARV: %s\nUSE1: %s",
+                    record.get("BL_ID"), record.get("BLNAME"), record.get("AREA_GROSS_INT"),
+                    record.get("ARV"), record.get("USE1"));
+
+            table.addCell(new Cell().add(new Paragraph(textData).setFont(font)));
+
+            String bl_id = (String) record.get("BL_ID");
+            File imageFile = new File("C:\\TMP\\itext\\" + bl_id + ".jpg");
+
+            if (imageFile.exists()) {
+                try {
+                    Image image = new Image(ImageDataFactory.create(imageFile.getAbsolutePath()));
+                    image.scaleToFit(200, 200);
+                    Cell imageCell = new Cell();
+                    imageCell.add(image);
+                    table.addCell(imageCell);
+                } catch (Exception e) {
+                    System.out.println("Error processing image for BL_ID: " + bl_id);
+                    e.printStackTrace();
+                    table.addCell(new Cell().add(new Paragraph("Invalid Image")));
+                }
+            } else {
+                table.addCell(new Cell().add(new Paragraph("No Image Available")));
+            }
+
+
+            count++;
+            if (count % 5 == 0) {
+                document.add(table);
+                table = new Table(2); // Reset table
+            }
+        }
+
+        // Ensure the last row of the table is complete.
+        while (count % 2 != 0) {
+            table.addCell(new Cell().add(new Paragraph(" ")));
+            count++;
+        }
+
+        if (count % 5 != 0) {
+            document.add(table);
+        }
         document.close();
 
         System.out.println("Pdf Created");
